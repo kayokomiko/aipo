8e3cdf44a1ea7e02836d97d0e50a83ecf268c260
kymr16 <kym.m1116gmail.com >
2015-07-06 18:37:41 +0900

    refs#34925

--- portlets/schedule/src/main/webapp/javascript/aipo/schedule/form.js
+++ portlets/schedule/src/main/webapp/javascript/aipo/schedule/form.js
@@ -23,6 +23,35 @@ dojo.require("aipo.widget.DropdownDatepicker");
 dojo.require("aipo.widget.MemberNormalSelectList");
 dojo.require("aipo.widget.GroupNormalSelectList");
 
+aipo.schedule.toggleMenu=function (node,filters,event){
+	var rect=filters.getBoundingClientRect();
+	var html=document.documentElement.getBoundingClientRect();
+	if (node.style.display == "none") {
+        dojo.query("div.menubar").style("display", "none");
+
+        var scroll={
+        	left:document.documentElement.scrollLeft||document.body.scrollLeft,
+        	top:document.documentElement.scrollTop||document.body.scrollTop
+        };
+        node.style.opacity="0";
+        setTimeout( function(){
+			dojo.style(node, "display" , "block");
+		}, 0);
+        if(html.right-node.clientWidth>rect.left){
+       		node.style.left=rect.left+scroll.left+"px";
+        }else{
+        	node.style.left=rect.right-node.clientWidth+scroll.left+"px";
+        }
+         if(html.bottom-node.clientHeight>rect.bottom||event){
+       		node.style.top=rect.bottom+scroll.top+"px";
+        }else{
+        	node.style.top=rect.top-node.clientHeight+scroll.top+"px";
+        }
+        node.style.opacity="";
+    } else {
+        dojo.query("div.menubar").style("display", "none");
+    }
+};
 aipo.schedule.setupTooltip = function(url, entityids, portlet_id) {
     ptConfig[portlet_id].isTooltipEnable = true;
 
